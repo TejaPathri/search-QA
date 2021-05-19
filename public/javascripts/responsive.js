673a93e2c9c4ab319aa953095d55b7c77e0163c3
pvsteja <tejapathri6@msitprogram.net>
2021-05-19 15:21:14 +0530

    first commit

--- public/javascripts/responsive.js
+++ public/javascripts/responsive.js
@@ -1,17 +1,17 @@
 // const axios = require('axios');
 // import axios from axios;
-async function myFunction() {
-    var txt = document.getElementById("text").value;
-    console.log(txt);
-    var url = "http://localhost:3000/qa/addQuestionWithAnswer";
-    axios.post(url, {
-        question: txt,
-        answer: ''
-      })
-      .then((response) => {
-        console.log(response);
-      }, (error) => {
-        console.log(error);
-      });
+// async function myFunction() {
+//     var txt = document.getElementById("text").value;
+//     console.log(txt);
+//     var url = "http://localhost:3000/qa/addQuestionWithAnswer";
+//     axios.post(url, {
+//         question: txt,
+//         answer: ''
+//       })
+//       .then((response) => {
+//         console.log(response);
+//       }, (error) => {
+//         console.log(error);
+//       });
 
-}
+// }
